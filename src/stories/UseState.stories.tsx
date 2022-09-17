7aac3522fa3807d8913b9439504a472c553a924d
fedorgerasimov <1031577@mail.ru>
2022-09-17 18:52:06 +0300

    20. useState

---
+++ src/stories/UseState.stories.tsx
@@ -0,0 +1,28 @@
+import React, {useMemo} from "react";
+import {useState} from "react";
+
+export default {
+    title: 'useState demo'
+}
+
+function generateData(){
+    console.log("generateData")
+    return 35535
+}
+
+export const Example1 = () => {
+    console.log("Example1")
+
+    //const initialValue= useMemo(generateData,[])
+    const [counter, setCounter] = useState(generateData)
+
+    const changer = (state: number) => {
+        debugger
+        return state + 1
+    }
+
+    return <>
+        <button onClick={() => setCounter(changer)}>+</button>
+        {counter}
+    </>
+}
